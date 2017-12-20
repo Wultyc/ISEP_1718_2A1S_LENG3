cbcf00df57e14cf78ff13af399e7ca2a551613bf
Jorge Gabriel Azevedo <jorge.gfm.azevedo@gmail.com>
2017-12-20 08:51:48 +0000

    Formatação do ficheiro README.md

--- src/leng3/LENG3.java
+++ src/leng3/LENG3.java
@@ -1,13 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package leng3;
+
+package UserInterface;
 
 /**
  *
- * @author sony
+ * @author Grupo 3
  */
 public class LENG3 {
 
