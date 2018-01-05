3ec76a26559be0036b727960867d6385a0f1b447
Maria Inês Rebelo Dias de Almeida <1160631@isep.ipp.pt>
2018-01-05 10:43:09 +0000

    Classes UI

---
+++ src/UserInterface/ListarOcupacaoUI.java
@@ -0,0 +1,35 @@
+package UserInterface;
+import java.util.Scanner;
+
+/**
+ *
+ * @author DEE
+ */
+public class ListarOcupacaoUI {
+    
+         
+    public static void main(String[] args) {
+        
+        String armazem;
+        double areaLogica, corredor;
+        
+        Scanner sc = new Scanner(System.in);    
+        //Listar armazens
+        System.out.println("Especifique Armazém: ");
+        armazem = sc.next();
+        
+        //Listar Areas Logicas
+        System.out.println("Especifique Aréa Lógica: ");
+        areaLogica = sc.nextDouble();
+        
+        //Listar Corredores
+        System.out.println("Especifique Corredor: ");
+        corredor = sc.nextDouble();
+        
+        //Listar Ocupação
+    }
+    
+    
+    
+    
+}
---
+++ src/UserInterface/OtimizarAtualizarGestaoUI.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package UserInterface;
+import java.util.Scanner;
+
+/**
+ *
+ * @author DEE
+ */
+public class OtimizarAtualizarGestaoUI {
+    
+    public static void main(String[] args) {
+        
+        String armazem, alteracoes;
+        double areaLogica, corredor;
+        
+        Scanner sc = new Scanner(System.in);
+        
+        //Listar armazens
+        System.out.println("Especifique Armazém: ");
+        armazem = sc.next();
+        
+        //Listar Areas Logicas
+        System.out.println("Especifique Aréa Lógica: ");
+        areaLogica = sc.nextDouble();
+        
+        //Listar Corredores
+        System.out.println("Especifique Corredor: ");
+        corredor = sc.nextDouble();        
+        
+        //Confere condições para o uso do AMPL
+        
+        //Envia os dados para o servidor 
+        
+        System.out.println("Pretende aplicar alterações ?");
+        alteracoes = sc.next();
+        
+        //Se sim -> sistema aplica alteracções
+        
+    }
+    
+    
+}
